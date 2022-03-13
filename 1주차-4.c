eb93dc83986fdbe780dbef179bec256cf4993fb2
helee777 <101447960+helee777@users.noreply.github.com>
2022-03-13 20:02:44 +0900

    Update 1주차-4.c

--- 1주차-4.c
+++ 1주차-4.c
@@ -12,14 +12,15 @@ int main() {
 	for (i = 0; p[i] != '\0'; i++);
 	size = i;
 
-
-	for (i = 0; i < size; i++) {
+	i = 0;
+	while (i < size) {
 		printf("%s\n", p);
 		for (int j = 0; j < size - 1; j++) {
 			temp = p[j];
 			p[j] = p[j + 1];
 			p[j + 1] = temp;
 		}
+		i++;
 	}
 
 	return 0;
